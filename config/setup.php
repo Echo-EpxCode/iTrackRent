1c776cb9d41e99c429978baa82f5c58852534077
EpEp <codillojonefer45@gmail.com>
2026-05-11 04:46:45 +0800

    refactor: update db

--- config/setup.php
+++ config/setup.php
@@ -7,7 +7,6 @@ $db_name = "iTrack_db";
 
 // ================= CONNECT =================
 $conn = new mysqli($host, $user, $pass);
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
