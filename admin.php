650886f79b8e05e8e9899182ebdfd7ab2f6a1542
EpEp <codillojonefer45@gmail.com>
2026-05-11 12:14:40 +0800

    refactor: update admin page

--- admin.php
+++ admin.php
@@ -80,7 +80,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <!-- Bootstrap 5 CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/css/form.css">
+    <link rel="stylesheet" href="assets/css/form.css">
 </head>
 
 <body>
