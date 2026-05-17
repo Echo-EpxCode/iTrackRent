485b2cd5a25a507534dfeac990461f4e2e40647e
EpEp <codillojonefer45@gmail.com>
2026-05-17 21:37:20 +0800

    Refactor: Add Note for Cancelation and Refund. Free Revision reached 2/2.

--- renter/my_reservations.php
+++ renter/my_reservations.php
@@ -3,7 +3,6 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// ================= DATABASE =================
 include_once __DIR__ . '/../config/setup.php';
 
 // ================= ACCESS CONTROL =================
